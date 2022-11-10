2d98e3f8d5e7806fcb739b8245c8b185831c64aa
aev <adan.vela@ucf.edu>
2022-11-10 01:22:34 -0500

    Allow cron string to be passed as an environmental variable

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script is required to overcome issues with cron loading in environmental variables.
 printenv > /etc/environment
+echo "$CRONSTRING root /usr/local/bin/python3 /rcvr.py >> /var/log/cron.log 2>&1" > /etc/cron.d/rcvr-cron
+chmod +x /etc/cron.d/rcvr-cron
 cron && tail -f /var/log/cron.log
